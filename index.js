af87fdbe768ddc3775a416b455b255e1e565ac74
piyxxx1 <piyushjha9001@gmail.com>
2025-11-15 13:21:25 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// desktop ui 
+
 let name = "hassan"
+
+
+
+// app interface
+
+const app = new App();
+
+
+    app.start();
+
+    app.stop();
+
+    app.restart();
+
+    app.shutdown();
+
+    app.reboot();
+
+    app.shutdown();
+
+    app.reboot();
